d8024be96aec8eb7b7dd82077ccd8b837eb8ae8e
amandajasmynee <putriamanda549@gmail.com>
2024-09-19 20:05:03 +0700

    Pertemuan 4 - Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,30 +9,14 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        // // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username', ' customer-1')->update($data); //update data user
-
-        // // coba akses model UserModel
-        // $user = UserModel::all(); // ambil semua data dari tabel m_user
-        // return view('user', ['data' => $user]);
-
-        $data = [
-            // 'level_id' => 2,
-            // 'username' => 'manager_dua',
-            // 'nama' => 'Manager 2',
-            // 'password' => Hash::make('12345')
-
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all();
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        // $user = UserModel::findOr(1, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }

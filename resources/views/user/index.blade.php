5ff3bd04e9fd126b0ff79cd915c22aca63119a44
amandajasmynee <putriamanda549@gmail.com>
2024-10-01 19:45:22 +0700

    Pertemuan 5 - Praktikum 4

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -14,6 +14,22 @@
     @if (session('error'))
         <div class="alert alert-danger">{{ session('error') }}</div>
     @endif
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group row">
+            <label class="col-1 control-label col-form-label">Filter:</label>
+            <div class="col-3">
+                <select class="form-control" id="level_id" name="level_id" required>
+                <option value="">- Semua</option>
+                @foreach($level as $item)
+                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                @endforeach
+                </select>
+                <small class="form-text text-muted">Level Pengguna</small>
+            </div>
+            </div>
+        </div>
+        </div>
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr>
@@ -40,6 +56,9 @@ $(document).ready(function() {
             "url": "{{ url('user/list') }}",
             "type": "POST",
             "dataType": "json",
+            "data": function (d) {
+                d.level_id = $('#level_id').val();
+            }
         },
         columns: [
            {
@@ -74,6 +93,9 @@ $(document).ready(function() {
             searchable: false
         }
         ]
+    });
+    $('#level_id').on('change', function() {
+        dataUser.ajax.reload();
     });
 });
 </script>

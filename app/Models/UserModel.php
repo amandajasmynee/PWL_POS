1a5dd436811a0f71e2b1274d735e7a0e583ff6dd
amandajasmynee <putriamanda549@gmail.com>
2024-10-12 22:53:13 +0700

    Pertemuan 7 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,15 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Autheticable; //implementasi class Authenticable
 
-class UserModel extends Model
+class UserModel extends Autheticable
 {
     use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $hidden = ['password']; //jangan di tampilkan saat select
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');

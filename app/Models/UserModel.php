b7c4c276d96e45e94b020bf70ce0a4546e930b90
amandajasmynee <putriamanda549@gmail.com>
2024-10-13 17:10:44 +0700

    Pertemuan 7 - Praktikum 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -18,7 +18,18 @@ class UserModel extends Autheticatable
     protected $hidden = ['password']; //jangan di tampilkan saat select
     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
+    // Relasi ke tabel level
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    // Mendapatkan nama role
+    public function getRoleName(): string {
+        return $this->level->level_nama;
+    }
+
+    // Cek apakah user memiliki role tertentu
+    public function hasRole($role): bool {
+        return $this->level->level_kode == $role;
     }
 }

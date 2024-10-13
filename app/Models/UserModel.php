e2bcaf34cd71329da232c8e4ba630047a57c58cb
amandajasmynee <putriamanda549@gmail.com>
2024-10-13 14:43:54 +0700

    [UPDATE] Pertemuan 7 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Autheticable; //implementasi class Authenticable
+use Illuminate\Foundation\Auth\User as Autheticatable; //implementasi class Authenticatable
 
-class UserModel extends Autheticable
+class UserModel extends Autheticatable
 {
     use HasFactory;
 
